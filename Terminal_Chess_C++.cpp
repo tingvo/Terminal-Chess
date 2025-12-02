27429df1e7d9029d202bb8675a09c0656b1a5590
tingvo <tom.ingvorsen@gmail.com>
2025-12-02 14:24:14 +0000

    Updated C++ version

---
+++ Terminal_Chess_C++.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string get_cell()
+string check_cell()
+
+void start_conditions(){
+
+}
+
+void move_cell{
+
+}
+
+string get_cell(){
+
+}
+
+string check_cell(){
+
+}
+
+void make_board(){
+
+}
+
+bool active = true;
+string rec_pieces [[],[]]
+char regions [['a','b','c','d','e','f','g','h'],['1','2','3','4','5','6','7','8']]
+int main(){
+    
+
+}
