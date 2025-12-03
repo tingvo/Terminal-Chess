df71cad2c488eeea9e7ae65f59320f0c2ed775e1
tingvo <tom.ingvorsen@gmail.com>
2025-12-03 08:53:06 +0000

    0_issue_fixed

--- Terminal_Chess_C++.cpp
+++ Terminal_Chess_C++.cpp
@@ -2,73 +2,227 @@
 #include <string>
 using namespace std;
 
-string get_cell()
-string check_cell()
+void start_conditions();
+string get_cell(string pc_sel);
+string def_cell(string piece);
+bool player1_move();
+bool player2_move();
+
+bool active = true;
+string rec_pieces[32][32];
+const string regions[8][8] = {{"a","b","c","d","e","f","g","h"},{"1","2","3","4","5","6","7","8"}};
+
+int main(){
+    start_conditions();
+    while (active == true){
+        active = player1_move();
+        if (active == false){
+            cout << "Player 2 wins!";
+            break;
+        }
+        active = player2_move();
+        if (active == false){
+            cout << "Player 1 wins!";
+            break;
+        }
+    }
+}
+
+string make_cell(string piece){
+    string output;
+    if (piece == "p1pawn"){
+        output = "|[p]";
+    } else if (piece == "p2pawn"){
+        output = "|(P)";
+    } else if (piece == "p1knight"){
+        output = "|[h]";
+    } else if (piece == "p2knight"){
+        output = "|(H)";
+    } else if (piece == "p1rook"){
+        output = "|[r]";
+    } else if (piece == "p2rook"){
+        output = "|(R)";
+    } else if (piece == "p1bishop"){
+        output = "|[b]";
+    } else if (piece == "p2bishop"){
+        output = "|(B)";
+    } else if (piece == "p1queen"){
+        output = "|[q]";
+    } else if (piece == "p2queen"){
+        output = "|(Q)";
+    } else if (piece == "p1king"){
+        output = "|[k]";
+    } else if (piece == "p2king"){
+        output = "|(K)";
+    } else {
+        output = "| 0 ";
+    }
+    return output;
+}
 
 void start_conditions(){
     cout << "| a | b | c | d | e | f | g | h |" << endl;
     int i = 0;
+    string piece;
+    //extern string rec_pieces[32][32];
     for (string y : regions[1]){
         for (string x : regions[0]){
             if (y == "2"){
-                string piece = "p2pawn";
+                piece = "p2pawn";
             } else if (y == "7"){
-                string piece = "p1pawn";
+                piece = "p1pawn";
             } else if ((x == "a" || x == "h") && y == "8"){
-                string piece = "p1rook";
+                piece = "p1rook";
             } else if ((x == "a" || x == "h") && y == "1"){
-                string piece = "p2rook";
+                piece = "p2rook";
             } else if ((x == "b" || x == "g") && y == "8"){
-                string piece = "p1knight";
+                piece = "p1knight";
             } else if ((x == "b" || x == "g") && y == "1"){
-                string piece = "p2knight";
+                piece = "p2knight";
             } else if ((x == "c" || x == "f") && y == "8"){
-                string piece = "p1bishop";
+                piece = "p1bishop";
             } else if ((x == "c" || x == "f") && y == "1"){
-                string piece = "p2bishop";
+                piece = "p2bishop";
             } else if (x == "d" && y == "8"){
-                string piece = "p1queen";
+                piece = "p1queen";
             } else if (x == "d" && y == "1"){
-                string piece = "p2queen";
+                piece = "p2queen";
             } else if (x == "e" && y == "8"){
-                string piece = "p1king";
+                piece = "p1king";
             } else if (x == "e" && y == "1"){
-                string piece = "p2king";
+                piece = "p2king";
             } else {
-                string piece = "0"
+                piece = "0";
             }
             if (piece != "0"){
                 rec_pieces[0][i] = x + y;
                 rec_pieces[1][i] = piece;
                 i++;
             }
-            //cellx = cell(piece);
-            //cout << cellx.region;
+            string cell = make_cell(piece);
+            cout << cell;
         }
         cout << "| " + y << endl;
     }
 }
 
-void move_cell{
-
+void move_cell(string sel_reg, string mov_reg, string piece){
+    //extern string rec_pieces[32][32];
+    int c = 0;
+    for (string rec_piece : rec_pieces[0]){
+        if (rec_piece == sel_reg){
+            rec_pieces[0][c] = mov_reg;
+        }
+        if (mov_reg == rec_piece){
+            rec_pieces[0][c] = "taken";
+            rec_pieces[1][c] = "taken";
+        }
+        c++;
+    }
 }
 
-string get_cell(){
-
-}
-
-string check_cell(){
-
+string get_cell(string pc_sel){
+    int c = 0;
+    bool present;
+    for (string rec_piece : rec_pieces[0]){
+        if (rec_piece == pc_sel){
+            present = true;
+            return rec_pieces[1][c];
+            break;
+        } else {
+            present = false;
+        }
+        c++;
+    }
+    if (present == false){
+        return "0";
+    }
 }
 
 void make_board(){
-
+    cout << "| a | b | c | d | e | f | g | h |" << endl;
+    for (string y : regions[1]){
+        for (string x : regions[0]){
+            string z = x + y;
+            string piece = get_cell(z);
+            string cell = make_cell(piece);
+            cout << cell;
+        }
+        cout << "| " + y << endl;
+    }
 }
 
-bool active = true;
-string rec_pieces[32][32]
-const string regions[8][8] {{"a","b","c","d","e","f","g","h"},{"1","2","3","4","5","6","7","8"}}
-int main(){
-    
+bool player1_move(){
+    bool valid1 = false;
+    bool valid2 = false;
+    string inp;
+    string pc_sel;
+    string mv_sel;
+    while (valid1 == false){
+        cout << "Player 1 what would you like to do?: ";
+        cin >> inp;
+        if (inp == "move"){
+            cout << "Player 1 please select a piece: ";
+            cin >> pc_sel;
+            string piece = get_cell(pc_sel);
+            if (piece == "0"){
+                cout << "No piece found, please select a piece.";
+            } 
+            //elif not pc_sel[0] in regions[0] or not pc_sel[1] in str(regions[1]):
+                //print('Please enter a valid coordinate.')
+            else {
+                valid1 = true;
+            }
+            while (valid2 == false){
+                cout << "Where to?: ";
+                cin >> mv_sel;
+                valid2 = true;
+                
+                //if not mv_sel[0] in regions[0] or not mv_sel[1] in str(regions[1]):
+                    //print('Please enter a valid coordinate.') 
+                //else {valid2 = true;}
+            } 
+            move_cell(pc_sel, mv_sel, piece);
+            make_board();
+    } else if (inp == "resign"){
+        return false;
+    } 
+    }
+}
 
+bool player2_move(){
+    bool valid1 = false;
+    bool valid2 = false;
+    string inp;
+    string pc_sel;
+    string mv_sel;
+    while (valid1 == false){
+        cout << "Player 2 what would you like to do?: ";
+        cin >> inp;
+        if (inp == "move"){
+            cout << "Player 2 please select a piece: ";
+            cin >> pc_sel;
+            string piece = get_cell(pc_sel);
+            if (piece == "0"){
+                cout << "No piece found, please select a piece.";
+            } 
+            //elif not pc_sel[0] in regions[0] or not pc_sel[1] in str(regions[1]):
+                //print('Please enter a valid coordinate.')
+            else {
+                valid1 = true;
+            }
+            while (valid2 == false){
+                cout << "Where to?: ";
+                cin >> mv_sel;
+                valid2 = true;
+                //if not mv_sel[0] in regions[0] or not mv_sel[1] in str(regions[1]):
+                    //print('Please enter a valid coordinate.') 
+                //else {valid2 = true;}
+            } 
+            move_cell(pc_sel, mv_sel, piece);
+            make_board();
+    } else if (inp == "resign"){
+        return false;
+    } 
+    }
 }
